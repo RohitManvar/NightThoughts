8fea3b794b41d14b4a8b8350fadb974fe1ee214f
Rohit Manvar <rohitmanvar123@gmail.com>
2026-05-30 20:52:21 +0530

    Fix Android build config - update SDK versions and remove reanimated

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
-import { StatusBar } from 'react-native';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { StatusBar, View } from 'react-native';
 import AppNavigator from './src/navigation/AppNavigator';
 import { initDatabase } from './src/database/db';
 
@@ -10,9 +9,9 @@ export default function App() {
   }, []);
 
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
+    <View style={{ flex: 1 }}>
       <StatusBar barStyle="light-content" backgroundColor="#0d0d1a" />
       <AppNavigator />
-    </GestureHandlerRootView>
+    </View>
   );
 }
